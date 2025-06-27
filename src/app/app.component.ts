3bd15b6421bce79078f8f99381d3297d3c6a602a
Chintan Kavathia <chintankumar.kavathia@siemens.com>
2025-06-27 19:41:14 +0530

    chore: move global style imports into style.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, ViewEncapsulation } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { HashLocationStrategy, Location, LocationStrategy } from '@angular/common';
 import packageInfo from 'projects/ngx-datatable/package.json';
 import { RouterLink, RouterOutlet } from '@angular/router';
@@ -6,9 +6,6 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // eslint-disable-next-line @angular-eslint/use-component-view-encapsulation
-  encapsulation: ViewEncapsulation.None,
-  styleUrls: ['./app.component.scss'],
   providers: [
     Location,
     {
