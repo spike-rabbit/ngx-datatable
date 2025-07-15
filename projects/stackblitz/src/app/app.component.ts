2c67d477db2aa02e3ffcbf68d7fe1b19bac766f9
Maximilian Koeller <maximilian.koeller@siemens.com>
2025-07-15 14:19:45 +0200

    chore(github): update and enable issue template

--- projects/stackblitz/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  imports: [],
-  template: ` <p> app works! </p> `,
-  styles: ``
-})
-export class AppComponent {}
